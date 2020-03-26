eca78241fbeb11b93d28323f83460c837b8247f4
dannyd216 <dparrish216@gmail.com>
2020-03-26 10:00:11 -0600

    finished.

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  (this.name = name), (this.age = age), (this.stomach = []);
 }
+
+Person.prototype.eat = function(someFood) {
+  if (this.stomach.length >= 10) {
+    return this.stomach;
+  } else {
+    this.stomach.push(someFood);
+  }
+};
+
+Person.prototype.poop = function() {
+  return (this.stomach = []);
+};
+
+Person.prototype.toString = function() {
+  return `${this.name}. ${this.age}`;
+};
 
 /*
   TASK 2
@@ -56,10 +72,29 @@ function Person() {
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
-
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons) {
+  return (this.tank += gallons);
+};
+
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+
+  if (this.tank < 0) {
+    this.odometer -= Math.abs(this.tank) * this.milesPerGallon;
+    this.tank = 0;
+  }
+
+  return `I ran out of fuel at ${this.odometer} miles`;
+};
+
 
 /*
   TASK 3
@@ -68,8 +103,16 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.favoriteToy = favoriteToy;
+  Person.call(this, name, age);
 
+  //this is subclassing of Person object in which Baby will inherit some traits from
+  Baby.prototype = Object.create(Person.prototype);
+
+  Baby.prototype.play = function() {
+    return `Playing with ${this.favoriteToy}`;
+  };
 }
 
 /* 
